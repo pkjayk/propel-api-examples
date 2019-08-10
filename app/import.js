8dba6d3f0b13f892a05008199d69392b090e66a5
Jayson Kadlecek <jayson@propelplm.com>
2019-08-09 22:13:06 -0700

    start express app for import

--- app/import.js
+++ app/import.js
@@ -1,55 +1,82 @@
 // Propel Import API
-// load items from CSV file into propel using API
+// load items from JSON file into propel using API
 
-const config = require('../config') // username and password stored here
-// ./config.js
-// module.exports = {
-//   username : 'ron@username.dev',
-//   password : 'yourpass_and_token', //must include password+token
-//   loginUrl : 'https://login.salesforce.com'
-// };
+  const config = require('../config') // username and password stored here
+  // ./config.js
+  // module.exports = {
+  //   username : 'jayson@username.dev',
+  //   password : 'yourpass_and_token', //must include password+token
+  //   loginUrl : 'https://login.salesforce.com'
+  // };
 
-const fs = require('fs')
-const jsforce = require('jsforce')        // https://www.npmjs.com/package/jsforce  and see docs: https://jsforce.github.io/
-const conn = new jsforce.Connection({ loginUrl : config.loginUrl})
-let namespace = config.namespace || 'PDLM';
+  const fs = require('fs')
+  const jsforce = require('jsforce')        // https://www.npmjs.com/package/jsforce  and see docs: https://jsforce.github.io/
+  const { parse } = require('json2csv')
+  const conn = new jsforce.Connection({ loginUrl : config.loginUrl})
+  let namespace = config.namespace || 'PDLM';
+  let data = require('../test_data/sample-bom-large.json')
 
 //
 // the file try.csv contains a valid CSV import file for your org
 // you must import a file using the Propel application one time to establish the column mappings
 // after that, this api will re-use those stored column to field mappings
 //
-const filedata = fs.readFileSync('../test_data/new-bom-import.csv', 'utf8');
-const base64data = new Buffer(filedata).toString('base64');
-conn.login(config.username, config.password)
-.then( () => {
-  /*
-     invoke the Propel API passing in the body base64 encoded
 
-     @apiParam [hasItem=true] indicates the file contains items
-     @apiParam [hasBom=true] indicates the file contains a BOM to import
-     @apiParam [hasAML=false] indicates the file contains Mfr parts to import
-     @apiParam [hasATT=false] indicates the file contains attachments to import
-     @apiParam [matchDescription=false] indicates the file contains items with no item number and descriptions should be used to locate items
-     @apiParam [isUpsertingItem=true] specify that the items in the file should overwrite items in the database if item number matches
-     @apiParam [isUpsertingBOM=true] specify that the BOM in the file should overwrite BOM in the database if item number matches
-     @apiParam [isReplacingBOM=false] specify that the BOM in the file should delete the BOM in the database if item number matches
-     @apiParam [isUpsertingAML=false] specify that the AML in the file should overwrite AML in the database if item number matches
-     @apiParam [isReplacingAML=false] specify that the AML in the file should delete the AML in the database if item number matches
+class Import {
 
-  */
-  return conn.apex.post('/services/apexrest/'+namespace+'/api/v2/import?isReplacingBOM=true', base64data);
-})
-.then( (resultRows) => {
-  console.log("Propel Item and Revision imported: ", resultRows);
-  // the response is list of Apex.RowResult
-  // [{ warning: '',
-  //   success: true,
-  //   rowName: '20-00362:A*',
-  //   objName: 'Item Revision',
-  //   isInsert: false,
-  //   id: 'a0936000009c7OaAAI',
-  //   errors: [],
-  //   description: '' }]
-})
-.catch((e)=>{ console.log(e) });
+  constructor(req, res) {
+
+    this.data = data
+
+    this.start()
+  }
+
+  start(){
+
+    let dataFile = this.data
+
+    let items = dataFile.bomTable.items;
+
+    // todo, make call to grab top-level assembly
+    var firstItem = {Level: 0, Category: 'Assembly', 'Item Number': null, Quantity: '1'}
+    var reformattedItems = [firstItem];
+
+    // iterate through BOM and grab relevant fields.
+    // TODO: Create mapping for fields to import
+    // currently hardcoding category — can be input in file instead.
+    for (i = 0; i < items.length; i++) {
+      
+      let newItem = items[i];
+
+      let level = (items[i].item.match(/\./g) || []).length + 1;
+
+      var thisItem = {};
+
+      thisItem = {Level: level, Category: 'Part', 'Item Number': newItem['partNumber'] ? newItem['partNumber'] : null, Quantity: newItem['quantity']}
+
+      reformattedItems.push(thisItem);
+
+    }
+
+    const fields = ['Level','Category','Item Number','Quantity'];
+    const opts = { fields };
+     
+    try {
+      const csv = parse(reformattedItems, opts);
+      const base64data = new Buffer(csv).toString('base64');
+
+      conn.login(config.username, config.password)
+      .then( () => {
+        return conn.apex.post('/services/apexrest/'+namespace+'/api/v2/import?isReplacingBOM=true', base64data);
+      })
+      .then( (resultRows) => {
+        console.log("Propel Item and Revision imported: ", resultRows);
+      })
+
+    .catch((e)=>{ console.log(e) });} catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+module.exports = Import
